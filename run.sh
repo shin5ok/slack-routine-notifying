a98677b9a4316f69158889149e638c94578ad5a5
shin5ok <shin5ok@55mp.com>
2023-08-07 18:41:19 +0900

    modify it

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 export SLACK_OAUTH_TOKEN=$(gcloud secrets versions access --secret=slack_oauth_token latest)
 export SLACK_CHANNEL_ID=$(gcloud secrets versions access --secret=slack_channel latest)
-export PATH=$PATH:$HOME/.pyenv/shims
+export TEMPLATE=template.txt
 
 export PYENV_ROOT="$HOME/.pyenv"
+export PATH=$PATH:$HOME/.pyenv/shims
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
-cd ~/slack-routine-notifying
+cd ~/repos/slack-routine-notifying
 poetry run python main.py
