2dc96c2e9caa724962063dd78672e3ad4c36362f
shin5ok <shin5ok@55mp.com>
2023-09-15 01:03:09 +0900

    just tidy up and work correctly

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
+export PROJECT_ID=shingo-ar-test0729
 export SLACK_OAUTH_TOKEN=$(gcloud --project=$PROJECT_ID secrets versions access --secret=slack_oauth_token latest)
 export SLACK_CHANNEL_ID=$(gcloud --project=$PROJECT_ID secrets versions access --secret=slack_channel latest)
 export TEMPLATE=template.txt
+
+echo "SLACK_OAUTH_TOKEN:" $SLACK_OAUTH_TOKEN
+echo "SLACK_CHANNEL_ID:" $SLACK_CHANNEL_ID
 
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH=$PATH:$HOME/.pyenv/shims
@@ -10,4 +14,12 @@ eval "$(pyenv init -)"
 # cli mode
 # cd ~/repos/slack-routine-notifying
 # poetry run python main.py
-docker run -d --restart always -p 8600:8080 -e SLACK_OAUTH_TOKEN=$SLACK_OAUTH_TOKEN -e SLACK_CHANNEL_ID=$SLACK_CHANNEL_ID -e TEMPLATE=$TEMPLATE slack-routing
+
+
+NAME=slack-routing
+docker build -t $NAME .
+docker stop $NAME
+docker rm $NAME
+CMD="docker run -d --name $NAME --restart always -p 8600:8080 -e SLACK_OAUTH_TOKEN=$SLACK_OAUTH_TOKEN -e SLACK_CHANNEL_ID=$SLACK_CHANNEL_ID -e TEMPLATE=$TEMPLATE $NAME"
+echo $CMD
+$CMD
