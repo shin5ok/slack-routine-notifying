21950a9b1d8e2815869d3c26a88d349d23c7af17
shin5ok <shin5ok@55mp.com>
2023-09-15 00:37:21 +0900

    change running itself to starting docker

--- run.sh
+++ run.sh
@@ -7,5 +7,7 @@ export PATH=$PATH:$HOME/.pyenv/shims
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
-cd ~/repos/slack-routine-notifying
-poetry run python main.py
+# cli mode
+# cd ~/repos/slack-routine-notifying
+# poetry run python main.py
+docker run -d --restart always -p 8600:8080 -e SLACK_OAUTH_TOKEN=$SLACK_OAUTH_TOKEN -e SLACK_CHANNEL_ID=$SLACK_CHANNEL_ID -e TEMPLATE=$TEMPLATE slack-routing
