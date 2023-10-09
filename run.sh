a26a40d5ecb1bef370fb6302d0feaf09d429da88
shin5ok <shin5ok@55mp.com>
2023-10-09 13:46:51 +0900

    switch LLM prompt by specifying template

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 export PROJECT_ID=$PROJECT_ID
 export TEMPLATE=template.txt
+export LLM_TEMPLATE=${LLM_TEMPLATE:-llm_template.txt}
 
 export SLACK_OAUTH_TOKEN=$(gcloud --project=$PROJECT_ID secrets versions access --secret=slack_oauth_token latest)
 export SLACK_CHANNEL_ID=$(gcloud --project=$PROJECT_ID secrets versions access --secret=slack_channel latest)
@@ -27,6 +28,6 @@ PORT=${SLACK_ROUTING_PORT:-8800}
 docker build -t $NAME .
 docker stop $NAME
 docker rm $NAME
-CMD="docker run -d --name $NAME --restart always -p $PORT:8080 -e SLACK_OAUTH_TOKEN=$SLACK_OAUTH_TOKEN -e SLACK_CHANNEL_ID=$SLACK_CHANNEL_ID -e TEMPLATE=$TEMPLATE -v $(pwd)/config:/config $NAME"
+CMD="docker run -d --name $NAME --restart always -p $PORT:8080 -e SLACK_OAUTH_TOKEN=$SLACK_OAUTH_TOKEN -e SLACK_CHANNEL_ID=$SLACK_CHANNEL_ID -e TEMPLATE=$TEMPLATE -e LLM_TEMPLATE=$LLM_TEMPLATE -v $(pwd)/config:/config $NAME"
 echo $CMD
 $CMD
